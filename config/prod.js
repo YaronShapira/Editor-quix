0fb1a63832e9c0c8497cc0c9edb0f4af89307966
ForeseN <yagosik@gmail.com>
2023-01-29 02:24:04 +0200

    changed db

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-    dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+    dbURL: 'mongodb+srv://harel714:harel714quix@cluster0.c5gzkwt.mongodb.net/?retryWrites=true&w=majority',
     dbName: 'quix_db',
 }
